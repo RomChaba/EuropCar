1106fd08411b38e1c70bc03285a88fc078a1d397
Romain <chabassier.r@gmail.com>
2018-04-09 17:26:33 +0200

    Floating button vers la pag des vehicule

--- app/src/main/java/ma/eni/fr/europcar/activity/LocationActivity.java
+++ app/src/main/java/ma/eni/fr/europcar/activity/LocationActivity.java
@@ -1,7 +1,10 @@
 package ma.eni.fr.europcar.activity;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import ma.eni.fr.europcar.R;
 import ma.eni.fr.europcar.fragment.LocationFragment;
@@ -11,6 +14,7 @@ import ma.eni.fr.europcar.service.LocationService;
 public class LocationActivity extends AppCompatActivity implements LocationFragment.OnFragmentInteractionListener {
 
     private LocationFragment fragment;
+    private FloatingActionButton btn_ajout_Location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,19 @@ public class LocationActivity extends AppCompatActivity implements LocationFragm
     protected void onResume() {
         super.onResume();
 
+        btn_ajout_Location = findViewById(R.id.btn_nouvelle_location);
+
         fragment = (LocationFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_location);
         fragment.refreshList(LocationService.getInstance().getLocationList());
+
+        btn_ajout_Location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LocationActivity.this,ListeVehiculeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
